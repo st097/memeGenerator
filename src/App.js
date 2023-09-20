d61a078429f5236ac9206149918240353aa4b79b
st097 <s.tozaj@gmail.com>
2023-09-20 22:42:18 +0200

    memeGenerator

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header.js"
+import Meme from "./components/Meme"
+
+
+export default function App(){
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
